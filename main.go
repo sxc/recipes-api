15aab8de9153fde8228cece17d6ea1189c13c5ca
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-04-07 20:18:28 +0800

    add route for CRUD

--- main.go
+++ main.go
@@ -27,6 +27,52 @@ func ListRecipesHanler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHanler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	for i, r := range recipes {
+		if r.ID == id {
+			recipes[i] = recipe
+			c.JSON(http.StatusOK, recipe)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{
+		"error": "recipe not found"})
+}
+
+func DeleteREcipeHanler(c *gin.Context) {
+	id := c.Param("id")
+	for i, r := range recipes {
+		if r.ID == id {
+			recipes = append(recipes[:i], recipes[i+1:]...)
+			c.JSON(http.StatusOK, gin.H{
+				"message": "recipe deleted"})
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{
+		"error": "recipe not found"})
+}
+
+func SearchRecipesHanler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+	for _, r := range recipes {
+		for _, t := range r.Tags {
+			if t == tag {
+				listOfRecipes = append(listOfRecipes, r)
+			}
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
+}
+
 var recipes []Recipe
 
 func init() {
@@ -39,6 +85,9 @@ func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHanler)
 	router.GET("/recipes", ListRecipesHanler)
+	router.PUT("/recipes/:id", UpdateRecipeHanler)
+	router.DELETE("/recipes/:id", DeleteREcipeHanler)
+	router.GET("/recipes/search", SearchRecipesHanler)
 	router.Run()
 }
 
